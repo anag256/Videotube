1b34bb5c979a604d401990f1d1b71a04d34294a5
anag.naikgaunekar <anag.naikgaunekar@thoughtworks.com>
2024-08-28 22:25:35 +0530

    Anag | Added vite cofig for docker nginx setup to work

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    proxy:{
-      '/api':{
-        target:'http://localhost:8000/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      }
-    }
-  }
-})
+  server: {
+    host: true, // Here
+    strictPort: true,
+    port: 5173,  //turn on for docker setup
+  },
+  // server:{
+  //   proxy:{
+  //     '/api':{
+  //       target:'http://localhost:8000/',
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api/, ''),
+  //     }
+  //   }
+  // }
+});
