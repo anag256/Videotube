1b57f55a4cb1ffdd56e4b3d0240095e7f2d50118
anag.naikgaunekar <anag.naikgaunekar@thoughtworks.com>
2024-09-24 21:23:01 +0530

    Enabled config for docker

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,23 +3,23 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   host: true, // Here
-  //   strictPort: true,
-  //   port: 5173,  //turn on for docker setup
-  // },
+  server: {
+    host: true, // Here
+    strictPort: true,
+    port: (process.env.VITE_PORT) as any,  //turn on for docker setup
+  },
   build: {
 		minify: true,
 	},
   base:'/',
-  server:{
-    proxy:{
-      '/api':{
-        target:'http://localhost:8000/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      }
-    }
-  }
+  // server:{
+  //   proxy:{
+  //     '/api':{
+  //       target:'http://localhost:8000/',
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api/, ''),
+  //     }
+  //   }
+  // }
 
 });
